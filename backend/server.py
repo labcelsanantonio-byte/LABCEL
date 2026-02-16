5700dee895532e89b5d5a81ce6943d087f69c99a
LABCEL <labcelsanantonio@gmail.com>
2026-02-16 16:22:42 -0600

    Elimina la dependencia 'Depends' de la importación en server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, HTTPException, UploadFile, File, Depends, Request, BackgroundTasks
+from fastapi import FastAPI, APIRouter, HTTPException, UploadFile, File, Request, BackgroundTasks
 from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
